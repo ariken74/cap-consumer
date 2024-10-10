2fe38a1b068cf611294ee6b3c9a1a61538308a1d
Andrea <ariken@gmail.com>
2024-10-10 13:02:27 +0200

    content type

--- src/utils.js
+++ src/utils.js
@@ -15,8 +15,8 @@ import { processors } from 'xml2js';
  * @ignore
  */
 export const DEFAULT_REQUEST_HEADERS = {
-  accept: 'application/xhtml+xml',
-  'content-type': 'application/xhtml+xml',
+  accept: 'text/xml',
+  'content-type': 'text/xml',
   'user-agent':
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
 };
