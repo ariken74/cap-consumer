f2459724bd425c72dde8eec901b5e9f40deb331e
Andrea <ariken@gmail.com>
2024-10-10 13:21:33 +0200

    esbuild

--- src/utils.js
+++ src/utils.js
@@ -15,8 +15,8 @@ import { processors } from 'xml2js';
  * @ignore
  */
 export const DEFAULT_REQUEST_HEADERS = {
-  accept: 'text/xml',
-  'content-type': 'text/xml',
+  accept: '*/*',
+  'content-type': 'application/xml',
   'user-agent':
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
 };
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -10,8 +10,8 @@ import {
 describe('utils', () => {
   it('should provide default request headers', () => {
     expect(DEFAULT_REQUEST_HEADERS).to.be.eql({
-      accept: 'text/xml',
-      'content-type': 'text/xml',
+      accept: '*/*',
+      'content-type': 'application/xml',
       'user-agent':
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
     });
